2aac76bd143ca28ea4acf65c5ea3bd0980e20dca
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-07-22 20:35:36 -0300

    feat: make "world" the default name when an empty string is passed to Hello

--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,22 @@ package main
 
 import "testing"
 
-func TestWorld(t *testing.T) {
-	got := Hello("Jake")
-	want := "Hello, Jake"
+func TestHelloWorld(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Jake")
+		want := "Hello, Jake"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
