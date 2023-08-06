0b553542cb2bdee6591d4b6cf82eaf5ec6ee961e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-08-05 21:32:27 -0300

    feat: finish collection of any size test

--- arraysAndSlices/arrays_and_alices.go
+++ arraysAndSlices/arrays_and_alices.go
@@ -1,6 +1,6 @@
 package arraysAndSlices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	total := 0
 
 	for _, number := range numbers { // range returns i and value
--- arraysAndSlices/arrays_and_alices_test.go
+++ arraysAndSlices/arrays_and_alices_test.go
@@ -3,11 +3,26 @@ package arraysAndSlices
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	sum := Sum(numbers)
-	expected := 15
 
-	if sum != expected {
-		t.Errorf("expected '%d' but got '%d' given '%v'", expected, sum, numbers)
-	}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5} // can also use [...]int{1, 2, 3, 4, 5}
+		sum := Sum(numbers)
+		expected := 15
+
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d' given '%v'", expected, sum, numbers)
+		}
+
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("wanted '%d' but got '%d', given '%v'", want, got, numbers)
+		}
+	})
 }
