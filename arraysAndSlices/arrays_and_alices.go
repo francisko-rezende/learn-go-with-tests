4bc65b10035950699f68b60132047efbfea9f30c
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-08-05 21:24:35 -0300

    refactor: use range in for loop

--- arraysAndSlices/arrays_and_alices.go
+++ arraysAndSlices/arrays_and_alices.go
@@ -3,8 +3,8 @@ package arraysAndSlices
 func Sum(numbers [5]int) int {
 	total := 0
 
-	for i := 0; i <= 4; i++ {
-		total += numbers[i]
+	for _, number := range numbers { // range returns i and value
+		total += number
 	}
 
 	return total
