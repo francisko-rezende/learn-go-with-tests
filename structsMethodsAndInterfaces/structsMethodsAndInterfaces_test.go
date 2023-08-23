1063ab9be9797172d02320f3734b297fd1bcbf7f
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-08-22 21:16:55 -0300

    test: finish test using methods for circle and rectangle

--- structsMethodsAndInterfaces/structsMethodsAndInterfaces_test.go
+++ structsMethodsAndInterfaces/structsMethodsAndInterfaces_test.go
@@ -3,19 +3,33 @@ package structsmethodsandinterfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	expected := 40.0
 
 	if got != expected {
-		t.Errorf("got %.2f but expected %2.f", got, expected) //The f is for our float64 and the .2 means print 2 decimal places.
+		t.Errorf("got %g but expected %2.f", got, expected) //The f is for our float64 and the .2 means print 2 decimal places.
 	}
 }
 
 func TestArea(t *testing.T) {
-	got := Area(5.0, 2.0)
-	expected := 10.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{5.0, 2.0}
+		got := rectangle.Area()
+		expected := 10.0
 
-	if got != expected {
-		t.Errorf("got %.2f but expected %.2f", got, expected)
-	}
+		if got != expected {
+			t.Errorf("got %g but expected %g", got, expected) // g prints more decimal places
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		expected := 314.1592653589793
+
+		if got != expected {
+			t.Errorf("got %g but expected %g", got, expected)
+		}
+	})
 }
