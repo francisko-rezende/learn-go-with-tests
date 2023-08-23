41976e10e7a73376b4f3de83bfdcef6dd9b925d8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-08-22 21:18:00 -0300

    chore: delete commented code

--- structsMethodsAndInterfaces/structsMethodsAndInterfaces.go
+++ structsMethodsAndInterfaces/structsMethodsAndInterfaces.go
@@ -22,11 +22,3 @@ func (c Circle) Area() float64 {
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
-
-// func Area(rectangle Rectangle) float64 {
-// 	return rectangle.Width * rectangle.Height
-// }
-
-// func Circle(r float64) float64 {
-// 	return math.Pi * r * r
-// }
