0eb09450887b2bd301b392fc3c78569fbe1db1fc
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-08-22 21:44:38 -0300

    test: implement test for triangle

--- structsMethodsAndInterfaces/structsMethodsAndInterfaces.go
+++ structsMethodsAndInterfaces/structsMethodsAndInterfaces.go
@@ -14,16 +14,25 @@ type Shape interface {
 // In Go interface resolution is implicit. If the type you pass in
 // matches what the interface is asking for, it will compile
 
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
-}
-
 type Circle struct {
 	Radius float64
 }
 
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func (c Circle) Area() float64 {
 	return c.Radius * c.Radius * math.Pi
+}
+
+func (t Triangle) Area() float64 {
+	return t.Base * t.Height / 2
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- structsMethodsAndInterfaces/structsMethodsAndInterfaces_test.go
+++ structsMethodsAndInterfaces/structsMethodsAndInterfaces_test.go
@@ -8,40 +8,56 @@ func TestPerimeter(t *testing.T) {
 	expected := 40.0
 
 	if got != expected {
-		t.Errorf("got %g but expected %2.f", got, expected) //The f is for our float64 and the .2 means print 2 decimal places.
+		t.Errorf("got %g but expected %g", got, expected) //The f is for our float64 and the .2 means print 2 decimal places.
 	}
 }
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{5.0, 2.0}, 10.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{5.0, 2.0}
-		checkArea(t, rectangle, 10.0)
+	// checkArea := func(t testing.TB, shape Shape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
 
-		// got := rectangle.Area()
-		// expected := 10.0
+	// t.Run("rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{5.0, 2.0}
+	// 	checkArea(t, rectangle, 10.0)
 
-		// if got != expected {
-		// 	t.Errorf("got %g but expected %g", got, expected) // g prints more decimal places
-		// }
-	})
+	// got := rectangle.Area()
+	// expected := 10.0
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-		// got := circle.Area()
-		// expected := 314.1592653589793
+	// if got != expected {
+	// 	t.Errorf("got %g but expected %g", got, expected) // g prints more decimal places
+	// }
+	// })
 
-		// if got != expected {
-		// 	t.Errorf("got %g but expected %g", got, expected)
-		// }
-	})
+	// t.Run("circles", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// 	// got := circle.Area()
+	// 	// expected := 314.1592653589793
+
+	// 	// if got != expected {
+	// 	// 	t.Errorf("got %g but expected %g", got, expected)
+	// 	// }
+	// })
 }
