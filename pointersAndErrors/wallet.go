295523d680285168bf7452767ea619d4bc8d3cf2
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-08-27 17:36:34 -0300

    refactor: add a Stringer interface for more informative print outputs

--- pointersAndErrors/wallet.go
+++ pointersAndErrors/wallet.go
@@ -2,16 +2,26 @@ package pointersanderrors
 
 import "fmt"
 
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // *Wallet rather than Wallet which you can read as "a pointer to a wallet".
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
--- pointersAndErrors/wallet_test.go
+++ pointersAndErrors/wallet_test.go
@@ -8,14 +8,15 @@ import (
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
+	// In Go, the fmt package provides formatting verbs like %s that are used to format values according to their respective Stringer interfaces. When you use %s in a format string, the fmt package looks for the String() method in the value you're trying to format and uses that method to get a string representation of the value.
 }
