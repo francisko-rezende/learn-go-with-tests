38ac2daca9f665694aa9503cea762aaaaa003a05
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-09-07 19:55:57 -0300

    test: add full string required for countdown test

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -5,6 +5,11 @@ import (
 	"io"
 )
 
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, "Go!")
 }
