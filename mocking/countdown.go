914b05eae012b53c539318f8cdf16da1ce2cd8ed
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-09-07 20:00:13 -0300

    refactor: move final word to a const

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -6,10 +6,11 @@ import (
 )
 
 const countdownStart = 3
+const finalWord = "Go!"
 
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
