8372a21668d501794e52c85772890909ffed94c8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-09-07 20:02:02 -0300

    feat: add timeout to print statements

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,6 +3,7 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 const countdownStart = 3
@@ -11,6 +12,7 @@ const finalWord = "Go!"
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Fprint(out, finalWord)
 }
