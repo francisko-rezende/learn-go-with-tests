e69374219d1036e9be5298e7b83925b309d806c3
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-09-16 21:05:30 -0300

    test: finish test for sleep call number and  order

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -10,6 +10,23 @@ type Sleeper interface {
 	Sleep()
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+const sleep = "sleep"
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+const write = "write"
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
 type SpySleeper struct {
 	Calls int
 }
