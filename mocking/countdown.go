cccee4d816283d7ab967c8cfa087900595f27c1f
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-09-16 21:20:59 -0300

    test: create tests for configurable sleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -10,6 +10,23 @@ type Sleeper interface {
 	Sleep()
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
 type SpyCountdownOperations struct {
 	Calls []string
 }
