e13a553f69d86093f5b4f30943ee8bec371c31f0
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-11-30 21:02:14 -0300

    chore: make it possible for the user to choose how many times the char will repeat

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, repeatNumber int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatNumber; i++ {
 		repeated += character
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,8 +3,8 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 3)
+	expected := "aaa"
 
 	if repeated != expected {
 		t.Errorf("Expected %q but got %q", expected, repeated)
@@ -13,6 +13,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(t *testing.B) {
 	for i := 0; i < t.N; i++ {
-		Repeat("a")
+		Repeat("a", 3)
 	}
 }
