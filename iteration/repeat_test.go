c213cb71876a493e74e86c7c68ab9e41a61a5c3b
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-11-30 21:04:07 -0300

    chore: create ExampleRepeat to document Repeat

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 3)
@@ -15,4 +18,10 @@ func BenchmarkRepeat(t *testing.B) {
 	for i := 0; i < t.N; i++ {
 		Repeat("a", 3)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b", 5)
+	fmt.Println(repeated)
+	// Output: "bbbbb"
 }
