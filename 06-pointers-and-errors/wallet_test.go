af152355718f458095333cc768de4bd9afa66cc5
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-24 19:46:20 -0300

    feat: implement Withdraw

--- 06-pointers-and-errors/wallet_test.go
+++ 06-pointers-and-errors/wallet_test.go
@@ -5,14 +5,27 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	depositAmount := Bitcoin(10)
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		depositAmount := Bitcoin(10)
 
-	wallet.Deposit(depositAmount)
-	got := wallet.GetBalance()
-	want := depositAmount
+		wallet.Deposit(depositAmount)
+		got := wallet.GetBalance()
+		want := depositAmount
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		wallet.Withdraw(5)
+		got := wallet.GetBalance()
+		want := Bitcoin(5)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
