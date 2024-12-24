640a9c8f3ab1ec93389186b1443d800d5cda6327
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-24 17:13:29 -0300

    refactor: use nested test runs for each case so it is easier to debug failing tests

--- structs-methods-and-interfaces/geometry_test.go
+++ structs-methods-and-interfaces/geometry_test.go
@@ -14,18 +14,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{shape: Rectangle{Width: 2.0, Heigth: 2.0}, want: 4.0},
-		{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
-		{shape: Triangle{Base: 2.0, Heigth: 5.0}, want: 5.0},
+		{name: "rectangle", shape: Rectangle{Width: 2.0, Heigth: 2.0}, want: 9.0},
+		{name: "circle", shape: Circle{Radius: 10.0}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 2.0, Heigth: 5.0}, want: 5.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+		})
 	}
 }
