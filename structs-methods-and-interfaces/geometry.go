36ad3d158872159a8b9e2ef141689d41105c2c4e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-24 16:43:11 -0300

    refactor: expand math.Pow call

--- structs-methods-and-interfaces/geometry.go
+++ structs-methods-and-interfaces/geometry.go
@@ -16,7 +16,7 @@ type Circle struct {
 }
 
 func (c Circle) Area() float64 {
-	return math.Pi * math.Pow(c.Radius, 2)
+	return math.Pi * (c.Radius * c.Radius)
 }
 
 func Perimeter(rectangle Rectangle) float64 {
