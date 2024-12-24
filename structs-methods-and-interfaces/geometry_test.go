04877e7336598e3e70b09354b29795b421c1dd10
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-24 16:49:50 -0300

    feat: create Shape interface

--- structs-methods-and-interfaces/geometry_test.go
+++ structs-methods-and-interfaces/geometry_test.go
@@ -13,24 +13,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{2.0, 2.0}
-		got := rectangle.Area()
-		want := 4.0
-
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{2.0, 2.0}
+		want := 4.0
+
+		checkArea(t, rectangle, want)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10.0}
-
-		got := circle.Area()
 		want := 314.1592653589793
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, want)
 	})
 }
