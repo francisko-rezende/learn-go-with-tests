d6667d886f2e7790867a6e766d93aafaf1d7efb0
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-24 22:10:06 -0300

    refactor: move error assertion to a new function

--- 06-pointers-and-errors/wallet_test.go
+++ 06-pointers-and-errors/wallet_test.go
@@ -5,26 +5,34 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	checkBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
 		t.Helper()
 		got := wallet.GetBalance()
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
+
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted error but didn't get one")
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		depositAmount := Bitcoin(10)
 		wallet.Deposit(depositAmount)
 		want := depositAmount
-		checkBalance(t, wallet, want)
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10)}
 		wallet.Withdraw(5)
 		want := Bitcoin(5)
-		checkBalance(t, wallet, want)
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("withdraw more than what the wallet has", func(t *testing.T) {
@@ -32,7 +40,8 @@ func TestWallet(t *testing.T) {
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(20))
 
-		checkBalance(t, wallet, startingBalance)
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
 
 		if err == nil {
 			t.Error("expected error but didn't get one")
