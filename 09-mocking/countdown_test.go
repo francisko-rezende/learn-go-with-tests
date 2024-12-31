e1d428d3b624596f4551036ba907d7c15b73de86
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-31 08:03:57 -0300

    test: create test for checking the order in which sleep and write are called

--- 09-mocking/countdown_test.go
+++ 09-mocking/countdown_test.go
@@ -2,6 +2,7 @@ package mocking
 
 import (
 	"bytes"
+	"slices"
 	"testing"
 )
 
@@ -13,22 +14,63 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+type SpyCountdownOperations struct {
+	Calls []string
+}
 
-	Countdown(buffer, spySleeper)
-	got := buffer.String()
-	want := `3
+const (
+	sleep = "sleep"
+	write = "write"
+)
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func TestCountdown(t *testing.T) {
+	t.Run("number of calls", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
+
+		Countdown(buffer, spySleeper)
+		got := buffer.String()
+		want := `3
 2
 1
 Go!`
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 
-	if spySleeper.Calls != 3 {
-		t.Errorf("noto enough calls to spySleeper, wanted 3, got %d", spySleeper.Calls)
-	}
+		if spySleeper.Calls != 3 {
+			t.Errorf("noto enough calls to spySleeper, wanted 3, got %d", spySleeper.Calls)
+		}
+	})
+
+	t.Run("call order", func(t *testing.T) {
+		spy := &SpyCountdownOperations{}
+
+		Countdown(spy, spy)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+		got := spy.Calls
+
+		if !slices.Equal(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
