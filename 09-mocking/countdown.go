803accb9432a361520155793c5e73537064e41fb
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-12-30 21:10:00 -0300

    refactor: use consts to avoid magic values in Countdown

--- 09-mocking/countdown.go
+++ 09-mocking/countdown.go
@@ -5,9 +5,14 @@ import (
 	"io"
 )
 
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
 func Countdown(writer io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(writer, i)
 	}
-	fmt.Fprint(writer, "Go!")
+	fmt.Fprint(writer, finalWord)
 }
