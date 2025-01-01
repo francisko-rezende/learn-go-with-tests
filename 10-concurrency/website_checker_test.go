bbf60d4a3b3593aec7c9dbf1f6f07f129e00b079
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-01-01 17:06:27 -0300

    test: create benchmark for website checker

--- 10-concurrency/website_checker_test.go
+++ 10-concurrency/website_checker_test.go
@@ -3,6 +3,7 @@ package concurrency
 import (
 	"maps"
 	"testing"
+	"time"
 )
 
 func mockWebsiteChecker(url string) bool {
@@ -26,5 +27,22 @@ func TestCheckWebsites(t *testing.T) {
 
 	if !maps.Equal(got, want) {
 		t.Fatalf("wanted %v got %v", want, got)
+	}
+}
+
+func slowStubWebsiteChecker(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
+	return true
+}
+
+func BenchmarkCheckWebsites(b *testing.B) {
+	urls := make([]string, 100)
+
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "some url"
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
 	}
 }
