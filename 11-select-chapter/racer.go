886b0ba8038a394efdfbfe1330f736184b19903c
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-02-07 21:30:27 -0300

    feat: use select to run the gets in parallel

--- 11-select-chapter/racer.go
+++ 11-select-chapter/racer.go
@@ -1,26 +1,25 @@
 package selectchapter
 
 import (
-	"fmt"
 	"net/http"
-	"time"
 )
 
 func Racer(slowUrl, fastUrl string) (winner string) {
-	slowUrlDuration := measureResponseTime(slowUrl)
-	fastUrlDuration := measureResponseTime(fastUrl)
-
-	fmt.Println(slowUrlDuration, fastUrlDuration)
-
-	if slowUrlDuration < fastUrlDuration {
+	select {
+	case <-ping(slowUrl):
 		return slowUrl
+	case <-ping(fastUrl):
+		return fastUrl
 	}
-
-	return fastUrl
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+
+	return ch
 }
