2be1ab0f319a95695e43721336185daca223ec43
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-03-20 20:41:36 -0300

    feat: write initial step of ConvertToRoman and its test

---
+++ 15-intro-to-property-based-tests/roman_numerals.go
@@ -0,0 +1,5 @@
+package introtopropertybasedtests
+
+func ConvertToRoman(arabicNumber int) string {
+	return "I"
+}
---
+++ 15-intro-to-property-based-tests/roman_numerals_test.go
@@ -0,0 +1,12 @@
+package introtopropertybasedtests
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+	got := ConvertToRoman(1)
+	want := "I"
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
