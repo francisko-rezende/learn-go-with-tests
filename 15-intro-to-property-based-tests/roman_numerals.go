5cab9b9314fd019c296b1f8cb6dbd1bd98fa9924
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-03-21 21:46:01 -0300

    feat: write test for converting 4 using ConvertToRoman

--- 15-intro-to-property-based-tests/roman_numerals.go
+++ 15-intro-to-property-based-tests/roman_numerals.go
@@ -3,6 +3,10 @@ package introtopropertybasedtests
 import "strings"
 
 func ConvertToRoman(arabicNumber int) string {
+	if arabicNumber == 4 {
+		return "IV"
+	}
+
 	var result strings.Builder
 
 	for i := 0; i < arabicNumber; i++ {
