f1b1d3e77c3a95d4023973bb8e006c73a00d9531
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-03-24 21:37:54 -0300

    feat: write test for 5

--- 15-intro-to-property-based-tests/roman_numerals.go
+++ 15-intro-to-property-based-tests/roman_numerals.go
@@ -6,6 +6,11 @@ func ConvertToRoman(arabicNumber int) string {
 	var result strings.Builder
 
 	for i := arabicNumber; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
+
 		if i == 4 {
 			result.WriteString("IV")
 			break
