dc7011f6480ec3ddfb786631925a1f0761f16c6e
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-03-28 21:50:26 -0300

    feat: finish initial clockface test

---
+++ 16-maths/clockface_test.go
@@ -0,0 +1,18 @@
+package maths_test
+
+import (
+	maths "learn-go-with-tests/16-maths"
+	"testing"
+	"time"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := maths.Point{X: 150, Y: 150 - 90}
+	got := maths.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}
